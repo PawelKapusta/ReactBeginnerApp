7af83add37a5f4f8b7b363af11ca4fa1063e5b2f
Paweł Kapusta <pawelkapusta70@gmail.com>
2020-09-01 15:12:54 +0200

    add toDo features

---
+++ src/Components/ToDoList/ToDo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ToDo(props){
+  return (
+
+      <div>
+
+        {
+          props.todo
+        }
+      </div>
+  )
+}
+
+export default ToDo;
--- src/Components/ToDoList/ToDoList.jsx
+++ src/Components/ToDoList/ToDoList.jsx
@@ -1,13 +1,42 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import ToDo from "./ToDo";
 
-class ToDoList extends Component {
-  render() {
-    return (
-        <div>
-          
-        </div>
-    );
+function ToDoList(props) {
+  const [toDoListState, setToDoListState] = useState({todos: [], inputValue: ""});
+  const handleInputChange = (event) => {
+    const {value} = event.target;
+    setToDoListState({
+      ...toDoListState,
+      inputValue: value
+    })
   }
+  const handleButtonClick = () => {
+    const { todos } = toDoListState;
+    setToDoListState({
+      todos: [...todos, inputValue],
+      inputValue: ""
+    })
+  }
+  const {todos, inputValue} = toDoListState;
+  return (
+      <div>
+        My application:
+        <input name="ToDo inout" type="text" placeholder="What we are gonna do today?" value={inputValue}
+               onChange={handleInputChange}/>
+        <button onClick={handleButtonClick}>
+          Add
+        </button>
+        {
+          todos.map((todo) => (
+              <ToDo
+                  todo={todo} key={todo}
+              />
+
+          ))
+        }
+      </div>
+  );
+
 }
 
 export default ToDoList;
